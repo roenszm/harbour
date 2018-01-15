63ce7c75ba27c2f6890708e7cc4d35688cce553f
roenszm <roenszm@163.com>
2018-01-15 09:40:24 +0800

    CI3.1.5

---
+++ application/views/main/index.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+	<title>roens</title>
+
+	<link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/main.css">
+
+	<script src="<?php echo base_url()?>assets/js/jquery-3.2.1.js"></script>
+	<script src="<?php echo base_url()?>assets/js/bootstrap.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo base_url()?>">Project name</a>
+		</div>
+		<div id="navbar" class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</div><!--/.nav-collapse -->
+	</div>
+</nav>
+
+<div class="container">
+	<div>
+		<h1>我的第一个程序 <?php echo $r;?></h1>
+		<p class="lead">
+            Use this document as a way to quickly start any new project.<br>
+            All you get is this text and a mostly barebones HTML document.
+        </p>
+	</div>
+</div>
+
+</body>
+</html>
