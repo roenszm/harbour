f3b0929d213ad93586c1ebc66bddb9e344eed4fc
roenszm <roenszm@163.com>
2018-01-17 00:03:46 +0800

    update gitignore and retrack files

--- application/views/photography/index.php
+++ application/views/photography/index.php
@@ -1,9 +1,5 @@
 <?php $this->load->view('common/header');?>
 	<div>
-		<h1>我的第一个程序</h1>
-		<p class="lead">
-            Use this document as a way to quickly start any new project.<br>
-            All you get is this text and a mostly barebones HTML document.
-        </p>
+
 	</div>
 <?php $this->load->view('common/footer');?>
